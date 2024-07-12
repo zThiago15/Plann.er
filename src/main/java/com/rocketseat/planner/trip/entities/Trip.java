cbb8cbc3d1c9da793c8a0eab743a1887bc2d4b03
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-07-12 15:46:05 -0300

    feat: create Trip entity

---
+++ src/main/java/com/rocketseat/planner/trip/entities/Trip.java
@@ -0,0 +1,53 @@
+package com.rocketseat.planner.trip.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Trips")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed",nullable = false)
+    private boolean isConfirmed;
+
+    @Column(name = "owner_name",nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email",nullable = false)
+    private String ownerEmail;
+
+    public Trip(TripRequestDTO data) {
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+    }
+}
+
