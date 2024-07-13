3c305107c3ee37cc1e4bff09d9798685f7c78e9b
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-07-13 17:26:45 -0300

    feat: create endpoint to the owner confirm a trip and trigger confirmation e-mail to participants

--- src/main/java/com/rocketseat/planner/controller/TripController.java
+++ src/main/java/com/rocketseat/planner/controller/TripController.java
@@ -60,4 +60,22 @@ public class TripController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable("id") UUID id) {
+        Optional<Trip> tripOptional = this.tripRepository.findById(id);
+
+        if (tripOptional.isPresent()) {
+            Trip rawTrip = tripOptional.get();
+            rawTrip.setConfirmed(true);
+
+            this.tripRepository.save(rawTrip);
+
+            this.participantService.triggerConfirmationEmailToParticipants(id);
+
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
